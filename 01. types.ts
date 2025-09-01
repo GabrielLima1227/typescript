6da95aa19203c9fbba19aa9511c53941cf1d545e
Gabriel de Lima Sousa <gablimax@hotmail.com>
2025-09-01 15:52:38 -0300

    feat: start learning TypeScript basics

---
+++ 01. types.ts
@@ -0,0 +1,8 @@
+const bootupMessage: string = "Starting support.ai servers...";
+console.log(bootupMessage);
+
+const supportAiLogs: number = 5;
+console.log(supportAiLogs);
+
+const supportAiLogs0 = "CRITICAL: Boots cut the power cord again";
+console.log(supportAiLogs);
