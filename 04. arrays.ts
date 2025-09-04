e37e17b86e2ef10cbe2c9c40671a9022a4965423
Gabriel de Lima Sousa <gablimax@hotmail.com>
2025-09-04 15:25:53 -0300

    feat: add examples of Arrays in TypeScript

---
+++ 04. arrays.ts
@@ -0,0 +1,33 @@
+export function averageScore(ratings: number[]): number {
+    if (ratings.length === 0) {
+        return 0;
+    }
+    
+    const somaTotal = ratings.reduce((sum, current) => sum + current, 0);
+    
+    return somaTotal / ratings.length;
+}
+
+// Heterogeneous Arrays
+export function interpolateComment(
+    id: number,
+    comment: string,
+    comments: (string | number)[]
+    ) {
+    const index = comments.findIndex(item => item === id);
+
+    if (index !== -1) {
+        comments[index] = comment;
+    }
+}
+
+// Rest Parameters
+export function formatLabels(...labels: string[]): string {
+    if (labels.length === 0) {
+        return "No Labels";
+    } else if (labels.length === 1) {
+        return `Label: ${labels[0]}`;
+    } else {
+        return `Labels: ${labels.join(", ")}`;
+    }
+}
