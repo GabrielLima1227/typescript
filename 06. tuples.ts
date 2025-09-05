226f42d35ec9438b3336991f5b4ba6b24a31d63b
Gabriel de Lima Sousa <gablimax@hotmail.com>
2025-09-05 13:47:26 -0300

    feat: add examples of Tuples in TypeScript

---
+++ 06. tuples.ts
@@ -0,0 +1,8 @@
+export function createTicket(
+      prevTicket: number,
+      comment: string,
+    ): [number, string, boolean] {
+      const newTicket = prevTicket + 1;
+      const isCritical = comment.toLowerCase().includes('critical');
+      return [newTicket, comment, isCritical];
+}
